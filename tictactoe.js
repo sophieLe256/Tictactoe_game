f426bc885dc76f659aea87598367ea090713fbe0
Sophie Le <dinokuro256@gmail.com>
2023-09-07 22:38:30 -0400

    update game

---
+++ tictactoe.js
@@ -0,0 +1,34 @@
+let turn = true;
+let clickedCount = 0;
+
+function play(btn){
+    btn.innerText = turn ? "X" : "O";
+    btn.classList.add(turn ? "xbtn" : "obtn");
+    btn.setAttribute("disabled", "disabled");
+
+    turn != turn;
+    clickedCount++
+
+    if(clickedCount == 16){
+        alert("Game over!!!")
+    }
+}
+
+function resetGame(){
+    if(!confirm('Do you want to reset the game?')){
+        return;
+    }
+
+    let btns = document.getElementsByClassName('btn')
+
+    for (const element of btns) {
+        element.innerText = ""
+        element.classList.remove("xbtn")
+        element.classList.remove("obtn")
+        element.removeAttribute("disabled")
+    }
+
+    turn = true;
+    clickedCount = 0; 
+
+}
